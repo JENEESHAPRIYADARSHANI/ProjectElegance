6cc79fa331cce8134e874e1022b72f52f40666db
Jeneesha Priyadarshani <135360330+JENEESHAPRIYADARSHANI@users.noreply.github.com>
2024-05-10 18:18:06 +0530

    Add files via upload

--- Database/eleganceeco.sql
+++ Database/eleganceeco.sql
@@ -1,1 +1,332 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2024 at 02:24 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eleganceeco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `phone`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin123', '0701112223');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cid` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cid`, `name`, `image`) VALUES
+(1, 'Mens', 'Mens.png'),
+(2, 'Kids', 'kids.png'),
+(6, 'Women clothings', 'cloths.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `orderid` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `paymentType` varchar(100) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordered_product`
+--
+
+CREATE TABLE `ordered_product` (
+  `oid` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` varchar(45) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `orderid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `price` varchar(20) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`pid`, `name`, `description`, `price`, `quantity`, `discount`, `image`, `cid`) VALUES
+(1, 'Men\'s T-shirt', 'Comfortable cotton t-shirt for men', '20.99', 100, 0, 'mens_tshirt.jpg', NULL),
+(10, 'Men Printed Casual Jacket', 'Color\r\nBlack\r\nFabric\r\nPure Cotton\r\nPattern\r\nPrinted\r\nStyle code\r\nAvengers_Zipper\r\nIdeal for\r\nMen\r\nSleeve\r\nFull Sleeve\r\nCharacter\r\nSuperman\r\nClosure\r\nZipper\r\nPack of\r\n1\r\nSuitable for\r\nWestern Wear\r\nReversible\r\nNo\r\nHooded\r\nYes\r\nType\r\nCasual Jacket\r\nFabric care\r\nMachine Wash as per Tag\r\nFit\r\nRegular', '1999.0', 1, 57, 'men_jacket.jpeg', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `registerdate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `address` varchar(250) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `pincode` varchar(10) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `name`, `email`, `password`, `phone`, `gender`, `registerdate`, `address`, `city`, `pincode`, `state`) VALUES
+(1, 'user', 'user@gmail.com', 'user123', '0784567888', 'Male', '2024-05-01 05:22:11', 'Nisalagiri Uyana', 'Horana', '12432', 'Western');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `idwishlist` int(11) NOT NULL,
+  `iduser` int(11) DEFAULT NULL,
+  `idproduct` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`idwishlist`, `iduser`, `idproduct`) VALUES
+(3, 1, 10);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uid_idx` (`uid`),
+  ADD KEY `pid_idx` (`pid`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId_idx` (`userId`);
+
+--
+-- Indexes for table `ordered_product`
+--
+ALTER TABLE `ordered_product`
+  ADD PRIMARY KEY (`oid`),
+  ADD KEY `orderid_idx` (`orderid`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pid`),
+  ADD KEY `cid_idx` (`cid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD UNIQUE KEY `phone_UNIQUE` (`phone`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`idwishlist`),
+  ADD KEY `idproduct_idx` (`idproduct`),
+  ADD KEY `iduser_idx` (`iduser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `ordered_product`
+--
+ALTER TABLE `ordered_product`
+  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `idwishlist` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `pid` FOREIGN KEY (`pid`) REFERENCES `product` (`pid`),
+  ADD CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES `user` (`userid`);
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `user` (`userid`);
+
+--
+-- Constraints for table `ordered_product`
+--
+ALTER TABLE `ordered_product`
+  ADD CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `order` (`id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `category` (`cid`);
+
+--
+-- Constraints for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD CONSTRAINT `idproduct` FOREIGN KEY (`idproduct`) REFERENCES `product` (`pid`),
+  ADD CONSTRAINT `iduser` FOREIGN KEY (`iduser`) REFERENCES `user` (`userid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
